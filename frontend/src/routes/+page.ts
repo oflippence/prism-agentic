5e1ca01e34177091f2a6ead9abe5227be2c800cb
oflippence <oflippence@gmail.com>
2025-02-21 10:12:37 +0000

    fix: remove prerender flags and switch to Vercel adapter

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,3 +1,2 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+// This page requires authentication, so we can't prerender it
+export const ssr = true;
